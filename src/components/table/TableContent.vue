54f354740a7726af35aceed1cc45b16bba2d33fa
PichardoWilfred <pichardowilfred@gmail.com>
2023-05-04 14:45:25 -0400

    vuex added & ui updated, delete item adjusted for lg displays

--- src/components/table/TableContent.vue
+++ src/components/table/TableContent.vue
@@ -8,17 +8,17 @@
             <input type="text" class="focus-within:outline-none w-[40%] px-3 py-2 bg-gray-1 rounded-sm">
         </div>
     </main>
-    <footer class="flex justify-center max-lg:flex-col bg-white p-4 border-t-2 border-gray-3 shadow-[0_1px_4px_rgba(0,0,0,0.16)]">
-        <div class="flex items-center justify-between">
+    <footer class="flex justify-center lg:justify-between max-lg:flex-col bg-white p-4 border-t-2 border-gray-3 shadow-[0_1px_4px_rgba(0,0,0,0.16)]">
+        <div class="max-lg:flex hidden items-center justify-between">
             <div class="flex">
-                <button class="flex items-center border-2 px-3 py-2 rounded-md border-gray-5 text-gray-5">
+                <button class="flex items-center border-2 px-3 py-2 rounded-md border-gray-5 text-gray-5 lg:mx-3 max-lg:mr-3">
                     <i class="fa-solid fa-xmark text-xl mr-3"></i>
                     <span class="text-md font-semibold">
                         Eliminar item
                     </span>
                 </button>
             </div>
-            <div class="flex flex-col lg:order-2 items-end ml-auto">
+            <div class="flex flex-col items-end">
                 <h4 class="text-gray-4 text-md font-bold">
                     TOTAL
                 </h4>
@@ -27,11 +27,28 @@
                 </h3>
             </div>
         </div>
-        <div class="flex lg:w-[380px] lg:items-center lg:order-1 mt-2 lg:mx-auto">
+        
+        <button class="hidden lg:flex items-center border-2 px-3 py-2 rounded-md border-gray-5 text-gray-5 lg:mx-3 max-lg:mr-3">
+            <i class="fa-solid fa-xmark text-xl mr-3"></i>
+            <span class="text-md font-semibold">
+                Eliminar item
+            </span>
+        </button>
+
+        <div class="flex lg:w-[380px] lg:items-center mt-2 lg:mx-auto">
             <input type="text" class="lg:h-[40px] pl-3 border-gray-4 border mr-2 flex-grow-[2] rounded-md focus-within:outline-none">
             <button class="bg-black hover:bg-black-2 text-white rounded-md flex-grow-2 ">
                 <i class="fa-solid fa-paper-plane p-3 px-5"></i>
             </button>
+        </div>
+
+        <div class="hidden lg:flex flex-col items-end">
+            <h4 class="text-gray-4 text-md font-bold">
+                TOTAL
+            </h4>
+            <h3 class="text-dark text-3xl font-bold">
+                100.00
+            </h3>
         </div>
     </footer>
 </template>
