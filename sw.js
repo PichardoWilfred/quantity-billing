33d683b84b3ac09c664b0acb22f73e4123357fb8
PichardoWilfred <pichardowilfred@gmail.com>
2023-06-24 15:02:39 -0400

    quantity billing

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
-      caches.open('fox-store').then((cache) => cache.addAll([
+    caches.open('fox-store').then((cache) => cache.addAll([
         '/pwa-examples/a2hs/',
         '/pwa-examples/a2hs/index.html',
         '/pwa-examples/a2hs/index.js',
@@ -9,12 +9,12 @@ self.addEventListener('install', (e) => {
         '/pwa-examples/a2hs/images/fox2.jpg',
         '/pwa-examples/a2hs/images/fox3.jpg',
         '/pwa-examples/a2hs/images/fox4.jpg',
-      ])),
+    ])),
     );
-  });
-  self.addEventListener('fetch', (e) => {
+});
+self.addEventListener('fetch', (e) => {
     console.log(e.request.url);
     e.respondWith(
-      caches.match(e.request).then((response) => response || fetch(e.request)),
+    caches.match(e.request).then((response) => response || fetch(e.request)),
     );
-  });
+});
